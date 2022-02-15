bb50ae2645ea8e0ed3b68625632f83b284385bcc
Nate Allen <nate.l.allen@gmail.com>
2022-02-14 22:58:51 -0500

    Added bootstrap , navbar and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
+  root 'home#index'
+
+  get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
